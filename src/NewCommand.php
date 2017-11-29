4cba4d1dee1a6335c482671175264518becbca22
王真一 <wangzhenyi@vpgame.cn>
2017-11-29 15:18:24 +0800

    update

---
+++ src/NewCommand.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Lumen\Installer\Console;
+
+use RuntimeException;
+use GuzzleHttp\Client;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class NewCommand extends Command
+{
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('new')
+            ->setDescription('Create a new Lumen application.')
+            ->addArgument('name', InputArgument::REQUIRED)
+            ->addArgument('remote', InputArgument::REQUIRED);
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param  InputInterface  $input
+     * @param  OutputInterface  $output
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        $this->verifyApplicationDoesntExist(
+            $directory = getcwd().'/'.$input->getArgument('name'),
+            $output
+        );
+
+        $remote = $input->getArgument('remote');
+        $output->writeln('<info>Crafting application...</info>');
+        exec('git clone '.$remote);
+        rename(getcwd().'/lumen',$input->getArgument('name'));
+        /*
+        $this->download($zipFile = $this->makeFilename())
+             ->extract($zipFile, $directory)
+             ->cleanUp($zipFile);
+
+        */
+
+        $output->writeln('<comment>Application ready! Build something amazing.</comment>');
+    }
+
+    /**
+     * Verify that the application does not already exist.
+     *
+     * @param  string  $directory
+     * @return void
+     */
+    protected function verifyApplicationDoesntExist($directory, OutputInterface $output)
+    {
+        if (is_dir($directory)) {
+            throw new RuntimeException('Application already exists!');
+        }
+    }
+
+    /**
+     * Generate a random temporary filename.
+     *
+     * @return string
+     */
+    protected function makeFilename()
+    {
+        return getcwd().'/lumen_'.md5(time().uniqid()).'.zip';
+    }
+
+    /**
+     * Download the temporary Zip to the given file.
+     *
+     * @param  string  $zipFile
+     * @return $this
+     */
+    protected function download($zipFile)
+    {
+        $response = (new Client)->get('http://cabinet.laravel.com/latest_lumen.zip');
+
+        file_put_contents($zipFile, $response->getBody());
+
+        return $this;
+    }
+
+    /**
+     * Extract the zip file into the given directory.
+     *
+     * @param  string  $zipFile
+     * @param  string  $directory
+     * @return $this
+     */
+    protected function extract($zipFile, $directory)
+    {
+        $archive = new ZipArchive;
+
+        $archive->open($zipFile);
+
+        $archive->extractTo($directory);
+
+        $archive->close();
+
+        return $this;
+    }
+
+    /**
+     * Clean-up the Zip file.
+     *
+     * @param  string  $zipFile
+     * @return $this
+     */
+    protected function cleanUp($zipFile)
+    {
+        @chmod($zipFile, 0777);
+
+        @unlink($zipFile);
+
+        return $this;
+    }
+}
